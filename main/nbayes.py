d54ba75af6045674c3df8ebedfed10a722fe1e31
huangtao <78929014@qq.com>
2018-04-28 00:16:27 +0800

    ff

--- main/nbayes.py
+++ main/nbayes.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('gbk')
 import numpy as np
 
 class NBayes(object):
@@ -31,7 +35,7 @@ class NBayes(object):
     # 生成普通的词频向量
     def calc_wordfreq(self, trainset):
         self.idf=np.zeros([1,self.vacablen])
-        self.tf=np.zeros([len(trainset),self.vacablen])
+        self.tf=np.zeros([self.doclength,self.vacablen])
         for indx in range(self.doclength):
             for word in trainset[indx]:
                 self.tf[indx][self.vacabulary.index(word)]+=1
@@ -42,9 +46,9 @@ class NBayes(object):
         self.tdm=np.zeros([len(self.Pcates),self.vacablen])
         sumlist=np.zeros([len(self.Pcates),1])
         for indx in range(self.doclength):
-            self.tdm[self.labels.index(indx)]+= self.tf[indx]
+            self.tdm[self.labels[indx]]+= self.tf[indx]
 
-            sumlist[self.labels.index(indx)]=np.sum(self.tdm[self.labels[indx]])
+            sumlist[self.labels[indx]]=np.sum(self.tdm[self.labels[indx]])
         self.tdm=self.tdm/sumlist
 
     def map2vocab(self, testdata):
@@ -61,6 +65,9 @@ class NBayes(object):
         predclass=""
         for tdm_vector,keyclass in zip(self.tdm,self.Pcates):
             temp=np.sum(testset*tdm_vector*self.Pcates[keyclass])
+            print testset,tdm_vector,self.Pcates[keyclass]
+            print temp
+            print "*"*10
             if temp>predvalue:
                 predvalue=temp
                 predclass=keyclass
@@ -68,4 +75,62 @@ class NBayes(object):
 
 
     def cal_tfidf(self, trainset):
-        pass
+        self.idf=np.zeros([1,self.vacablen])
+        self.tf=np.zeros([self.doclength,self.vacablen])
+        for indx in range(self.doclength):
+            for word in trainset[indx]:
+                self.tf[indx,self.vacabulary.index(word)]+=1
+            self.tf[indx]=self.tf[indx]/float(len(trainset[indx]))
+            for sigleword in set(trainset[indx]):
+                self.idf[0,self.vacabulary.index(sigleword)]+=1
+        self.idf=np.log(float(self.doclength)/self.idf)
+        self.tf=np.multiply(self.tf,self.idf)
+
+
+if __name__ == '__main__':
+    trainset=[['你','吃饭','了','吗'],
+              ['你','喝水','了','吗'],
+              ['你', '睡醒', '了', '吗']]
+
+    nb=NBayes()
+    nb.train_set(trainset,[0,1,1])
+    nb.cal_tfidf(trainset)
+    print nb.idf
+    print nb.tf
+    print ' '.join(nb.vacabulary)
+    nb.map2vocab(trainset[0])
+    print nb.predict(nb.testset)
+    # nb.map2vocab(trainset[1])
+    # print nb.predict(nb.testset)
+    # nb.map2vocab(trainset[2])
+    # print nb.predict(nb.testset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
