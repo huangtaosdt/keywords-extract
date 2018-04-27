9d8e74b279097e1a60be026526b7b7a941725b64
huangtao <huangtaosdt@163.com>
2018-04-27 21:51:46 +0800

    04/27

--- main/nbayes.py
+++ main/nbayes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class NBayes(object):
     def __init__(self):
         self.vacabulary = []
@@ -28,17 +30,42 @@ class NBayes(object):
 
     # 生成普通的词频向量
     def calc_wordfreq(self, trainset):
-        pass
-
+        self.idf=np.zeros([1,self.vacablen])
+        self.tf=np.zeros([len(trainset),self.vacablen])
+        for indx in range(self.doclength):
+            for word in trainset[indx]:
+                self.tf[indx][self.vacabulary.index(word)]+=1
+            for sigleword in set(trainset[indx]):
+                self.idf[0,self.vacabulary.index(sigleword)]+=1
 
     def build_tdm(self):
-        pass
+        self.tdm=np.zeros([len(self.Pcates),self.vacablen])
+        sumlist=np.zeros([len(self.Pcates),1])
+        for indx in range(self.doclength):
+            self.tdm[self.labels.index(indx)]+= self.tf[indx]
+
+            sumlist[self.labels.index(indx)]=np.sum(self.tdm[self.labels[indx]])
+        self.tdm=self.tdm/sumlist
 
     def map2vocab(self, testdata):
-        pass
+        self.testset=np.zeros([1,self.vacablen])
+        for word in testdata:
+            # 应该加 try
+            self.testset[0,self.vacabulary.index(word)]+=1
 
     def predict(self, testset):
-        pass
+        if np.shape(testset)[1]!=self.vacablen:
+            print "输入错误"
+            exit(0)
+        predvalue=0
+        predclass=""
+        for tdm_vector,keyclass in zip(self.tdm,self.Pcates):
+            temp=np.sum(testset*tdm_vector*self.Pcates[keyclass])
+            if temp>predvalue:
+                predvalue=temp
+                predclass=keyclass
+        return predclass
+
 
     def cal_tfidf(self, trainset):
         pass
